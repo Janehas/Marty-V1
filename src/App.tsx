2a0914ebc689d4d23e713c55bd76969c880385f5
Jane Hasbany <jane@MacBook-Air-de-Jane.local>
2026-07-03 16:20:07 +0200

    description de ta modif

--- src/App.tsx
+++ src/App.tsx
@@ -1744,7 +1744,10 @@ export default function App() {
                       {/* Click overlay feedback */}
                       <div className="absolute inset-0 bg-black/0 group-hover/viewfinder:bg-black/25 flex items-center justify-center transition-all z-10">
                         <div className="bg-black/70 backdrop-blur-xs text-white px-2.5 py-1 rounded-lg text-[9px] font-bold flex items-center gap-1 opacity-0 group-hover/viewfinder:opacity-100 transition-opacity">
-                          <Camera className="w-3.5 h-3.5 text-[#FF5C00]" />
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-camera w-3.5 h-3.5 text-[#FF5C00]" aria-hidden="true">
+                            <path d="M13.997 4a2 2 0 0 1 1.76 1.05l.486.9A2 2 0 0 0 18.003 7H20a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h1.997a2 2 0 0 0 1.759-1.048l.489-.904A2 2 0 0 1 10.004 4z"></path>
+                            <circle cx="12" cy="13" r="3"></circle>
+                          </svg>
                           Cliquez pour capturer
                         </div>
                       </div>
